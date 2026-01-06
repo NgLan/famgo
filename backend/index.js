4d232abc37402e7520bc893307dae990da6b9df2
NgLan <ngoclan271204@gmail.com>
2026-01-06 19:23:46 +0700

    fix auth logic

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.enable('trust proxy');
 const database = require("./config/database.js");
 const bodyParser = require("body-parser");
 const cors = require("cors");
